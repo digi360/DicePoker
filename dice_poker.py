104a564ba1e5d6647692d5c20872069de4364ec1
digi360 <sydney.sedibe@umuzi.org>
2018-11-30 17:55:45 +0200

    Add message for program end.

--- dice_poker.py
+++ dice_poker.py
@@ -1,12 +1,11 @@
 # Graphics program to roll a pair of dice. Uses custom widgets
 # Button and DieView
-
-from random import randrange
 from graphics import GraphWin, Point, Rectangle, Text
 
 from button import Button
 from dieview import DieView
 
+from random import randrange
 from collections import Counter
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 500
@@ -18,13 +17,8 @@ global scoreText
 hand = []
 
 def rollDice():
-	# for k in range(5):
-	# 	hand[k].setValue(randrange(1, 7))
-	hand[0].setValue(2)
-	hand[1].setValue(1)
-	hand[2].setValue(3)
-	hand[3].setValue(1)
-	hand[4].setValue(5)
+	for k in range(5):
+		hand[k].setValue(randrange(1, 7))
 	helpText.setText("Rolling cost: -$10")
 
 def playHand():
@@ -38,31 +32,29 @@ def checkHand():
 	output = helpText.getText() + "\n"
 	output += str(hand[0].getValue()) + " | " + str(hand[1].getValue()) + " | " + str(hand[2].getValue()) + " | " + str(hand[3].getValue()) + " | " + str(hand[4].getValue()) + "\n"
 
-	# if isFiveOfAKind():
-	# 	output += "Five of a kind: You win $30"
-	# 	playerAmount += 30
-	# elif isFourOfAKind():
-	# 	output += "Four of a kind: You win $15"
-	# 	playerAmount += 15
-	# elif isFullHouse():
-	# 	output += "Full House: You win $12"
-	# 	playerAmount += 15
-	# elif isStraight():
-	# 	output += "Straight: You win $20"
-	# 	playerAmount += 15
-	# elif isThreeOfAKind():
-	# 	output += "Three Of A Kind: You win $8"
-	# 	playerAmount += 15
-	# elif isTwoPair():
-	# 	output += "Two Pairs: You win $5"
-	# 	playerAmount += 15
-	# elif isOnePair():
-	# 	output += "One Pair: You win $0"
-	# 	playerAmount += 15
-	# else:
-	# 	output += "No wins in this roll."
+	if isFiveOfAKind():
+		output += "Five of a kind: You win $30"
+		playerAmount += 30
+	elif isFourOfAKind():
+		output += "Four of a kind: You win $15"
+		playerAmount += 15
+	elif isFullHouse():
+		output += "Full House: You win $12"
+		playerAmount += 12
+	elif isStraight():
+		output += "Straight: You win $20"
+		playerAmount += 20
+	elif isThreeOfAKind():
+		output += "Three Of A Kind: You win $8"
+		playerAmount += 8
+	elif isTwoPair():
+		output += "Two Pairs: You win $5"
+		playerAmount += 5
+	elif isOnePair():
+		output += "One Pair: You win $0"
+	else:
+		output += "No wins in this roll."
 
-	output += "isOnePair() = " + isOnePair()
 
 	helpText.setText(output)
 
@@ -85,12 +77,12 @@ def isTwoPair():
 	return ( Counter(Counter(sorted(handValues())).values()).values() == [1, 2])
 
 def isOnePair():
-	return str( Counter(sorted(handValues())).values() ) # == [1, 1, 1, 2])
+	return ( sorted(Counter(sorted(handValues())).values()) == [1, 1, 1, 2])
 
 def handValues():
 	return [hand[0].getValue(), hand[1].getValue(), hand[2].getValue(), hand[3].getValue(), hand[4].getValue()]
 
-def updateScreen(scoreText):
+def updateScore(scoreText):
 	scoreText.setText("Score: $" + str(playerAmount))
 
 def main():
@@ -111,12 +103,12 @@ def main():
 		hand.append(DieView(win, Point(WINDOW_WIDTH / 2 - m, WINDOW_HEIGHT / 2), 100))
 		m -= 125
 
-	scoreText = Text(Point(50, 25), "Score Text Goes Here...")
+	scoreText = Text(Point(50, 50), "Score Text Goes Here...")
 	scoreText.setTextColor("white")
 	scoreText.draw(win)
 
 	global helpText
-	helpText = Text(Point(WINDOW_WIDTH - 200, 25), "Help Text Goes Here...")
+	helpText = Text(Point(WINDOW_WIDTH - 200, 75), "Help Text Goes Here...")
 	helpText.setTextColor("white")
 	helpText.draw(win)
 
@@ -125,11 +117,19 @@ def main():
 	# event loop
 	run = True
 	while run:
-		updateScreen(scoreText)
+		updateScore(scoreText)
 		clickPoint = win.getMouse()
 		if (roll.clicked(clickPoint)):
 			playHand()
-		run = ( (not quit.clicked(clickPoint)) and (playerAmount > 0) )
+		run = ( (not quit.clicked(clickPoint)) and (playerAmount >= 10) )
+	updateScore(scoreText)
+
+	# By now, player has exhausted funds
+	if (playerAmount <= 10):
+		helpText.setText(helpText.getText() + "\nYou've don't have enough for another roll.")
+		roll.deactivate()
+		while (not quit.clicked(clickPoint)): # Wait till the user clicks quit
+			clickPoint = win.getMouse()
 
 	# close the window
 	win.close()
