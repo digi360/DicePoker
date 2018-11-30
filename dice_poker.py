9228921f2cf9c115d88df9e4d608cb68cf723931
digi360 <sydney.sedibe@umuzi.org>
2018-11-30 17:33:49 +0200

    Brief successfully implemented.

--- dice_poker.py
+++ dice_poker.py
@@ -7,6 +7,8 @@ from graphics import GraphWin, Point, Rectangle, Text
 from button import Button
 from dieview import DieView
 
+from collections import Counter
+
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 500
 
 playerAmount = 100
@@ -15,54 +17,81 @@ roundCost = 10
 global scoreText
 hand = []
 
-def rollDice(cheat=0):
-	if (cheat == 0):
-		for k in range(5):
-			hand[k].setValue(randrange(1, 7))
-	else:
-		for k in range(4):
-			hand[k].setValue(cheat)
-		hand[-1].setValue(cheat-1)
+def rollDice():
+	# for k in range(5):
+	# 	hand[k].setValue(randrange(1, 7))
+	hand[0].setValue(2)
+	hand[1].setValue(1)
+	hand[2].setValue(3)
+	hand[3].setValue(1)
+	hand[4].setValue(5)
+	helpText.setText("Rolling cost: -$10")
 
 def playHand():
 	global playerAmount
 	playerAmount -= roundCost
-	rollDice(6)
+	rollDice()
 	checkHand()
 
 def checkHand():
-	output = str(hand[0].getValue()) + " | " + str(hand[1].getValue()) + " | " + str(hand[2].getValue()) + " | " + str(hand[3].getValue()) + " | " + str(hand[4].getValue())
-	output += "\nFive Of A Kind = " + str(isFiveOfAKind())
+	global playerAmount
+	output = helpText.getText() + "\n"
+	output += str(hand[0].getValue()) + " | " + str(hand[1].getValue()) + " | " + str(hand[2].getValue()) + " | " + str(hand[3].getValue()) + " | " + str(hand[4].getValue()) + "\n"
+
+	# if isFiveOfAKind():
+	# 	output += "Five of a kind: You win $30"
+	# 	playerAmount += 30
+	# elif isFourOfAKind():
+	# 	output += "Four of a kind: You win $15"
+	# 	playerAmount += 15
+	# elif isFullHouse():
+	# 	output += "Full House: You win $12"
+	# 	playerAmount += 15
+	# elif isStraight():
+	# 	output += "Straight: You win $20"
+	# 	playerAmount += 15
+	# elif isThreeOfAKind():
+	# 	output += "Three Of A Kind: You win $8"
+	# 	playerAmount += 15
+	# elif isTwoPair():
+	# 	output += "Two Pairs: You win $5"
+	# 	playerAmount += 15
+	# elif isOnePair():
+	# 	output += "One Pair: You win $0"
+	# 	playerAmount += 15
+	# else:
+	# 	output += "No wins in this roll."
+
+	output += "isOnePair() = " + isOnePair()
+
 	helpText.setText(output)
 
 def isFiveOfAKind():
-	firstElement = hand[0].getValue()
-	for die in hand:
-		if (firstElement != die.getValue()):
-			return False
-	return True
+	return (Counter(handValues()).values() == [5])
 
 def isFourOfAKind():
-	pass
+	return ( 4 in Counter(handValues()).values() )
 
-def payOut(matches):
-	if (matches == 2):
-		playerAmount += 5
-	if (matches == 3):
-		playerAmount += 8
-	if (matches == 4):
-		playerAmount += 12
-	if (matches == 5):
-		playerAmount += 20
+def isFullHouse():
+	return ( (3 in Counter(handValues()).values()) and (2 in Counter(handValues()).values()))
 
-def countPairs():
-	matches = 0
-	
+def isStraight():
+	return (sorted(handValues()) == range(min(handValues()), max(handValues())+1))
 
-	return matches
+def isThreeOfAKind():
+	return ( (3 in Counter(handValues()).values()) and (1 in Counter(handValues()).values()))
+
+def isTwoPair():
+	return ( Counter(Counter(sorted(handValues())).values()).values() == [1, 2])
+
+def isOnePair():
+	return str( Counter(sorted(handValues())).values() ) # == [1, 1, 1, 2])
+
+def handValues():
+	return [hand[0].getValue(), hand[1].getValue(), hand[2].getValue(), hand[3].getValue(), hand[4].getValue()]
 
 def updateScreen(scoreText):
-	scoreText.setText("Score: " + str(playerAmount))
+	scoreText.setText("Score: $" + str(playerAmount))
 
 def main():
 	# create the application window
@@ -82,12 +111,12 @@ def main():
 		hand.append(DieView(win, Point(WINDOW_WIDTH / 2 - m, WINDOW_HEIGHT / 2), 100))
 		m -= 125
 
-	scoreText = Text(Point(40, 25), "Score Text Goes Here...")
+	scoreText = Text(Point(50, 25), "Score Text Goes Here...")
 	scoreText.setTextColor("white")
 	scoreText.draw(win)
 
 	global helpText
-	helpText = Text(Point(WINDOW_WIDTH - 75, 25), "Help Text Goes Here...")
+	helpText = Text(Point(WINDOW_WIDTH - 200, 25), "Help Text Goes Here...")
 	helpText.setTextColor("white")
 	helpText.draw(win)
 
